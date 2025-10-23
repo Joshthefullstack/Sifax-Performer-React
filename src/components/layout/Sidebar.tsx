14bb92d85a62c4d389e51bcdf367daa72ebce539
Joshthefullstack <ibiamjoshua20@gmail.com>
2025-10-23 09:27:25 +0100

    new changes

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -44,9 +44,9 @@ const Sidebar: React.FC = () => {
 
   return (
     <aside
-      id="layout-menu"
+      id={sidebarExpanded ? "layout-menu" : "layout-menu-sm"}
       className={`layout-menu menu-vertical menu bg-menu-theme transition-all duration-200 ease-in-out ${
-        sidebarExpanded ? "width-big" : "width-small"
+        sidebarExpanded ? "width-big" : "width-sm"
       }`}
       onMouseEnter={() => collapsed && setHovered(true)}
       onMouseLeave={() => collapsed && setHovered(false)}>
@@ -94,6 +94,7 @@ const Sidebar: React.FC = () => {
                 <path
                   d="M14.3584 11.8336C14.0654 12.1266 14.0654 12.6014 14.3584 12.8944L18.071 16.607C18.4615 16.9975 18.4615 17.6305 18.071 18.021C17.6805 18.4115 17.0475 18.4115 16.657 18.021L11.6819 13.0459C11.3053 12.6693 11.3053 12.0587 11.6819 11.6821L16.657 6.707C17.0475 6.31653 17.6805 6.31653 18.071 6.707C18.4615 7.09747 18.4615 7.73053 18.071 8.121L14.3584 11.8336Z"
                   fillOpacity="0.4"
+                  fill="#fff"
                 />
               </g>
             </svg>
@@ -103,15 +104,18 @@ const Sidebar: React.FC = () => {
               width="24"
               height="24"
               viewBox="0 0 24 24"
-              fill="none"
+              color="#fff"
+              // className="collapse"
               xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M8.47365 11.7183C8.11707 12.0749 8.11707 12.6531 8.47365 13.0097L12.071 16.607C12.4615 16.9975 12.4615 17.6305 12.071 18.021C11.6805 18.4115 11.0475 18.4115 10.657 18.021L5.83009 13.1941C5.37164 12.7356 5.37164 11.9924 5.83009 11.5339L10.657 6.707C11.0475 6.31653 11.6805 6.31653 12.071 6.707C12.4615 7.09747 12.4615 7.73053 12.071 8.121L8.47365 11.7183Z"
                 fillOpacity="0.9"
+                color="#fff"
               />
               <path
                 d="M14.3584 11.8336C14.0654 12.1266 14.0654 12.6014 14.3584 12.8944L18.071 16.607C18.4615 16.9975 18.4615 17.6305 18.071 18.021C17.6805 18.4115 17.0475 18.4115 16.657 18.021L11.6819 13.0459C11.3053 12.6693 11.3053 12.0587 11.6819 11.6821L16.657 6.707C17.0475 6.31653 17.6805 6.31653 18.071 6.707C18.4615 7.09747 18.4615 7.73053 18.071 8.121L14.3584 11.8336Z"
                 fillOpacity="0.4"
+                color="#fff"
               />
             </svg>
           )}
@@ -126,7 +130,7 @@ const Sidebar: React.FC = () => {
           className={`active ${
             sidebarExpanded ? "menu-item" : "menu-small-active"
           }`}>
-          <a href="index.html" className="menu-link">
+          <a href="/" className="menu-link">
             <i className="menu-icon tf-icons ri-dashboard-line"></i>
             {/* <div data-i18n="Dashboard">Dashboard</div> */}
             {sidebarExpanded && <div data-i18n="Dashboard">Dashboard</div>}
@@ -333,7 +337,7 @@ const Sidebar: React.FC = () => {
           </a>
         </li>
 
-            {/* Settings */}
+        {/* Settings */}
         <li className={`${sidebarExpanded ? "menu-item" : "menu-small"}`}>
           <a
             href="#"
