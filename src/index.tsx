5458a0d8f7513c055248aba7e5b0bc2dff724698
Joshthefullstack <ibiamjoshua20@gmail.com>
2025-10-29 22:43:36 +0100

    working on the manage supervision section: still needs some tuning

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./assets/vendor/libs/tagify/tagify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
